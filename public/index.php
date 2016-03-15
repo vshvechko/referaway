7000c0e579a72856b04f970acbebe27f556a3687
vshvechko <vladimir.shvechko@gmail.com>
2016-03-15 03:44:29 +0300

    update file structure

--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
 
-require_once __DIR__ . '/vendor/autoload.php';
-
-$app = new Slim\App(include __DIR__ . '/config/settings.php');
+$app = new Slim\App(include __DIR__ . '/../config/settings.php');
 
 (new \App\Configurator())->loadDependencyDefaults($app->getContainer())
     ->initMiddleware($app)
