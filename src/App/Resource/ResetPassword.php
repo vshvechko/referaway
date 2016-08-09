f674159ff656ce6b5ec3f7896919f7db243e1641
vshvechko <vladimir.shvechko@gmail.com>
2016-08-09 23:07:41 +0300

    reset password service

---
+++ src/App/Resource/ResetPassword.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Resource;
+
+use App\DAO\UserDAO;
+use App\Exception\StatusException;
+use Respect\Validation\Exceptions\ValidationException;
+use Respect\Validation\Validator as v;
+
+class ResetPassword extends AbstractResource
+{
+
+    /**
+     * @var UserDAO
+     */
+    private $userService;
+
+    public function init() {
+        $this->userService = new UserDAO($this->getEntityManager());
+    }
+
+
+    public function post($id = null)
+    {
+        try {
+            $data = $this->getRequest()->getParsedBody();
+            $this->addValidator('email', v::email()->length(null, 32));
+            $this->validateArray($data);
+
+            $user = $this->userService->findByEmail($data['email']);
+            if (!$user) {
+                throw new StatusException('Not Found', self::STATUS_NOT_FOUND);
+            }
+
+            $pass = $this->getServiceLocator()->get('encryptionHelper')->generatePassword();
+            $encoded = $this->getServiceLocator()->get('encryptionHelper')->getHash($pass);
+            $user->setPassword($encoded);
+            $this->userService->save($user);
+
+            $emailManager = $this->getServiceLocator()->get('mailManager');
+
+            try {
+                $emailManager->sendPasswordEmail($user->getEmail(), $pass);
+            } catch (\Exception $e) {
+                $logger = $this->getServiceLocator()->get('logger');
+                $logger->err(sprintf('%s in %s:%s', $e->getMessage(), $e->getFile(), $e->getLine()));
+                $logger->debug($e->getTraceAsString());
+            }
+        } catch (ValidationException $e) {
+            throw new StatusException($e->getMainMessage(), self::STATUS_BAD_REQUEST);
+        }
+    }
+}
